059bc655298674cbb06723e6fd0b1dd099b883e2
SilviaRP31 <92043710+SilviaRP31@users.noreply.github.com>
2024-02-08 11:06:28 +0100

    Update FileLanguageFilter.java

--- lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -1,5 +1,13 @@
 package edu.upf.filter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Optional;
+import edu.upf.parser.SimplifiedTweet;
+
+
 public class FileLanguageFilter {
     final String inputFile;
     final String outputFile;
@@ -7,5 +15,24 @@ public class FileLanguageFilter {
     public FileLanguageFilter(String inputFile, String outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+    }
+    public void filterLanguage(String language) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) { 
+                Optional<SimplifiedTweet> optionalTweet = SimplifiedTweet.fromJson(line);
+                
+                if (optionalTweet.isPresent()) {
+                    SimplifiedTweet tweet = optionalTweet.get();
+                    
+                    if (tweet.getLanguage().equals(language)) {
+                        writer.write(line);
+                        writer.newLine();
+                    }
+                }
+            }
+        }
     }
 }
