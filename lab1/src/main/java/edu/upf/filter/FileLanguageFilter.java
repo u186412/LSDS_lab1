af5facf62f2982f30a267502d90f81b62ccbcac9
guilirancel <115788459+guilirancel@users.noreply.github.com>
2024-02-08 10:54:45 +0100

    Update FileLanguageFilter.java

--- lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -1,6 +1,15 @@
 package edu.upf.filter;
 
-public class FileLanguageFilter {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+
+import edu.upf.parser.SimplifiedTweet;
+
+public class FileLanguageFilter implements LanguageFilter{
     final String inputFile;
     final String outputFile;
 
@@ -8,4 +17,28 @@ public class FileLanguageFilter {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
     }
+    
+    @Override
+    public void filterLanguage(String language) throws Exception {
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+            //Read each line from the input file 
+            String jline;
+            while ((jline = reader.readLine()) != null){
+                Optional<SimplifiedTweet> optionalTweet = SimplifiedTweet.fromJson(jline);
+
+                if (optionalTweet.isPresent() && optionalTweet.get().getLanguage().equals(language)) {
+                    // Write the tweet to the output file
+                    writer.write(jline);
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            // If an IO exception occurs, throw a new exception with a meaningful message
+            throw new Exception("Error occurred while filtering tweets: " + e.getMessage());
+        } 
+    }
 }
+
