be4d8a08d4b481581f2aabfd61d81114bb950e25
u186412 <simon.gasion01@estudiant.upf.edu>
2024-02-15 13:24:27 +0100

    Uploader implementation (NOT TESTED)

--- lab1/src/main/java/edu/upf/TwitterFilter.java
+++ lab1/src/main/java/edu/upf/TwitterFilter.java
@@ -1,26 +1,29 @@
 package edu.upf;
 
-//import java.edu.upf.filter.FileLanguageFilter;
-//import java.edu.upf.uploader.S3Uploader;
+import edu.upf.filter.FileLanguageFilter;
+import edu.upf.uploader.S3Uploader;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class TwitterFilter {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws Exception {
         List<String> argsList = Arrays.asList(args);
         String language = argsList.get(0);
         String outputFile = argsList.get(1);
         String bucket = argsList.get(2);
+        String accessKey = argsList.get(3);
+        String secretKey = argsList.get(4);
+        String sessionToken = argsList.get(5);
         System.out.println("Language: " + language + ". Output file: " + outputFile + ". Destination bucket: " + bucket);
         for(String inputFile: argsList.subList(3, argsList.size())) {
             System.out.println("Processing: " + inputFile);
-            //final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
-            //filter.filterLanguage(language);
+            final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
+            filter.filterLanguage(language);
         }
-
-        //final S3Uploader uploader = new S3Uploader(bucket, "prefix", "upf");
-        //uploader.upload(Arrays.asList(outputFile));
+        final S3Uploader uploader = new S3Uploader(bucket, language, accessKey, secretKey, sessionToken);
+        uploader.upload(Arrays.asList(outputFile));
     }
 }
