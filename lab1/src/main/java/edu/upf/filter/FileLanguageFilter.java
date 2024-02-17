c1e211ed06ae1c4ce9d0a02e38c78e4aee352c88
Silvia Riaño <silvia.riano01@estudiant.upf.edu>
2024-02-17 19:38:17 +0100

    update filter

--- lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Optional;
-
 import edu.upf.parser.SimplifiedTweet;
 
 public class FileLanguageFilter implements LanguageFilter{
@@ -20,25 +19,42 @@ public class FileLanguageFilter implements LanguageFilter{
     
     @Override
     public void filterLanguage(String language) throws Exception {
+        long startTime = System.currentTimeMillis();
+        int totalTweetsprocessed = 0;
+        
         try{
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true));
 
             //Read each line from the input file 
             String jline;
             while ((jline = reader.readLine()) != null){
+                long tweetStartTime = System.currentTimeMillis();
                 Optional<SimplifiedTweet> optionalTweet = SimplifiedTweet.fromJson(jline);
 
                 if (optionalTweet.isPresent() && optionalTweet.get().getLanguage().equals(language)) {
                     // Write the SIMPLIFIED tweet to the output file
                     writer.write(optionalTweet.toString());
                     writer.newLine();
+                    totalTweetsprocessed++;
                 }
+                long tweetEndTime = System.currentTimeMillis();
+                long tweetProcessingTime = tweetEndTime - tweetStartTime;
+                
+                System.out.println("Tweet processed in " + tweetProcessingTime + " milliseconds");
+            
             }
         } catch (IOException e) {
             // If an IO exception occurs, throw a new exception with a meaningful message
             throw new Exception("Error occurred while filtering tweets: " + e.getMessage());
         } 
+        
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        
+        System.out.println("Total tweets processed: " + totalTweetsprocessed);
+        System.out.println("Total processing time: " + totalTime + " milliseconds");
+    
     }
 }
 
