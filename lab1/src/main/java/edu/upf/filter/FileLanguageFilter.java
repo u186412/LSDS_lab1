3286cd5f5cad280321da8d9bbcd3754cdb544875
guilirancel <115788459+guilirancel@users.noreply.github.com>
2024-02-18 13:26:30 +0100

    Update FileLanguageFilter.java final

--- lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -6,55 +6,49 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Optional;
+
 import edu.upf.parser.SimplifiedTweet;
 
 public class FileLanguageFilter implements LanguageFilter{
     final String inputFile;
     final String outputFile;
+    int tweetsFiltered;
 
     public FileLanguageFilter(String inputFile, String outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+        // Initialize a counter for tweets filtered
+        this.tweetsFiltered = 0;
     }
     
     @Override
     public void filterLanguage(String language) throws Exception {
-        long startTime = System.currentTimeMillis();
-        int totalTweetsprocessed = 0;
-        
         try{
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true)); // set to true to append the new language and not rewrite the file 
 
             //Read each line from the input file 
             String jline;
             while ((jline = reader.readLine()) != null){
-                long tweetStartTime = System.currentTimeMillis();
                 Optional<SimplifiedTweet> optionalTweet = SimplifiedTweet.fromJson(jline);
 
                 if (optionalTweet.isPresent() && optionalTweet.get().getLanguage().equals(language)) {
                     // Write the SIMPLIFIED tweet to the output file
                     writer.write(optionalTweet.toString());
                     writer.newLine();
-                    totalTweetsprocessed++;
+                    // Increment the counter for each tweet filtered
+                    tweetsFiltered++; 
                 }
-                long tweetEndTime = System.currentTimeMillis();
-                long tweetProcessingTime = tweetEndTime - tweetStartTime;
-                
-                System.out.println("Tweet processed in " + tweetProcessingTime + " milliseconds");
-            
             }
         } catch (IOException e) {
             // If an IO exception occurs, throw a new exception with a meaningful message
             throw new Exception("Error occurred while filtering tweets: " + e.getMessage());
         } 
-        
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        
-        System.out.println("Total tweets processed: " + totalTweetsprocessed);
-        System.out.println("Total processing time: " + totalTime + " milliseconds");
-    
+    }
+
+    // Method to get the number of tweets filtered
+    public int getTweetsFiltered() {
+        return tweetsFiltered;
     }
 }
 
